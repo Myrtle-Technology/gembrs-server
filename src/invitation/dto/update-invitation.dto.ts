2b1b3690afc4d18d3501d58aee3f585571a8373d
Busola Okeowo <busolaokemoney@gmail.com>
2022-10-26 02:12:58 +0100

    update current organization

--- src/invitation/dto/update-invitation.dto.ts
+++ src/invitation/dto/update-invitation.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateInvitationDto } from './create-invitation.dto';
 
-export class UpdateInvitationDto extends PartialType(CreateInvitationDto) {
-  _id?: string;
-}
+export class UpdateInvitationDto extends PartialType(CreateInvitationDto) {}
